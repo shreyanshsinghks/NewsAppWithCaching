3090438061c84459b31e9b823a04068b775b5874
Shreyansh <shreyanshsinghks@gmail.com>
2024-07-15 02:36:27 +0530

    Project setup completed

---
+++ app/src/main/java/com/shreyanshsinghks/newsappfk/di/NetworkModule.kt
@@ -0,0 +1,10 @@
+package com.shreyanshsinghks.newsappfk.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+
+@Module
+@InstallIn
+class NetworkModule {
+
+}
---
+++ app/src/main/java/com/shreyanshsinghks/newsappfk/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.shreyanshsinghks.newsappfk.utils
+
+const val BASE_URL = "https://newsapi.org/v2/"
